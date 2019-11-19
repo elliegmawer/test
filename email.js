cafb2b29426cf6bb672911f293b55a5fde84d4a9
Ubuntu <ubuntu@ip-172-31-95-18.ec2.internal>
2019-11-19 18:41:44 +0000

    first commit

---
+++ email.js
@@ -0,0 +1,19 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "developer_weather_app", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.email.value,
+            "project_enquiry": contactForm.projectsummary.value
+        })
+        .then(function(response) {
+            console.log("SUCCESS!", response.status, response.text);
+        }, function(error) {
+            console.log("FAILED...", error);
+        });
+    return false; // To block from loading a new page
+}
+
+//modal
+$("#emailform").on('submit', function(e) {
+    $('#myModal').modal('show');
+    e.preventDefault();
+});
